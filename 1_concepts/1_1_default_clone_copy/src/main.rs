82256c879485e8c8ae219950624d30ad427df2cb
Nikita Maslov <maslov.n.e@gmail.com>
2023-07-10 17:39:05 +0300

    finilize task

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,29 @@
+pub type Coordinate = f64;
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct Point {
+    pub x: Coordinate,
+    pub y: Coordinate,
+}
+
+#[derive(Debug, Clone)]
+pub struct Polyline(Vec<Point>);
+
+impl Polyline {
+    pub fn new(points: Vec<Point>) -> Self {
+        if points.is_empty() {
+            panic!("there should be at least one point in the set of points");
+        }
+
+        Self(points)
+    }
+
+    pub fn points(&self) -> &[Point] {
+        &self.0
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let polyline = Polyline::new(vec![Default::default(), Default::default()]);
+    println!("{:?}", polyline);
 }
