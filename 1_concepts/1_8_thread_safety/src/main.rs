eedc05d62b9d2921c2155c6040db7805c235bd1e
Nikita Maslov <maslov.n.e@gmail.com>
2023-07-18 23:23:49 +0300

    switch to using static_assertions

--- 1_concepts/1_8_thread_safety/src/main.rs
+++ 1_concepts/1_8_thread_safety/src/main.rs
@@ -1,41 +1,23 @@
+use static_assertions::{
+    assert_impl_all, assert_impl_one, assert_not_impl_all, assert_not_impl_any,
+};
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::sync::{Mutex, MutexGuard};
-use std::thread;
+use std::sync::MutexGuard;
 
-#[derive(Debug)]
 struct OnlySync<'a>(MutexGuard<'a, String>);
-
-#[derive(Debug)]
 struct OnlySend(RefCell<String>);
-
-#[derive(Debug)]
 struct SyncAndSend(i32);
-
-#[derive(Debug)]
 struct NotSyncNotSend(Rc<i32>);
 
 fn main() {
-    let mutex = Mutex::new(String::from("hello"));
+    assert_impl_one!(OnlySync: Sync);
+    assert_not_impl_any!(OnlySync: Send);
 
-    let only_sync = OnlySync(mutex.lock().unwrap());
-    let only_send = OnlySend(RefCell::new(String::from("hello")));
-    let sync_and_send = SyncAndSend(8);
-    let _not_sync_not_send = NotSyncNotSend(Rc::new(8));
+    assert_not_impl_any!(OnlySend: Sync);
+    assert_impl_one!(OnlySend: Send);
 
-    thread::scope(|s| {
-        s.spawn(|| {
-            dbg!(&only_sync); // works, because OnlySend is Sync
-                              // dbg!(only_sync); // won't compile, because OnlySend isn't Send
+    assert_impl_all!(SyncAndSend: Sync, Send);
 
-            // dbg!(&only_send); // won't compile, because OnlySend isn't Sync
-            dbg!(only_send); // works, because OnlySend is Send
-
-            dbg!(&sync_and_send); // works, because SyncAndSend is Sync
-            dbg!(sync_and_send); // works, because SyncAndSend is Send
-
-            // dbg!(&_not_sync_not_send); // won't compile, because NotSyncNotSend isn't Sync
-            // dbg!(_not_sync_not_send); // won't compile, because NotSyncNotSend isn't Send
-        });
-    });
+    assert_not_impl_all!(NotSyncNotSend: Sync, Send);
 }
