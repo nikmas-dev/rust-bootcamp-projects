4d79497049c0c6e0fae6f91c5006418fcd8e8ba8
Nikita Maslov <maslov.n.e@gmail.com>
2023-07-19 13:04:22 +0300

    finilize task_1_9

--- 1_concepts/1_9_phantom/src/main.rs
+++ 1_concepts/1_9_phantom/src/main.rs
@@ -1,3 +1,55 @@
+use rand::prelude::SliceRandom;
+use std::marker::PhantomData;
+
+pub trait GetRandomFact {
+    fn get_random_fact() -> String;
+}
+
+struct Fact<T: GetRandomFact>(PhantomData<T>);
+
+impl<T: GetRandomFact> Fact<T> {
+    pub fn get() -> String {
+        T::get_random_fact()
+    }
+}
+
+impl<T> GetRandomFact for Vec<T> {
+    fn get_random_fact() -> String {
+        ["vec is heap-allocated", "vec may re-allocate on growing"]
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .to_string()
+    }
+}
+
 fn main() {
     println!("Implement me!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn should_return_random_fact_about_vec() {
+        let mut got_facts = HashSet::new();
+
+        let mut i = 0;
+
+        loop {
+            let fact = Fact::<Vec<i32>>::get();
+            got_facts.insert(fact);
+
+            if got_facts.len() > 1 {
+                break;
+            }
+
+            if i > 100_000_000 {
+                panic!("random facts aren't random enough");
+            }
+
+            i += 1;
+        }
+    }
 }
