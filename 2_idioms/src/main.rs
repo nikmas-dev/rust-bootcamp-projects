963a3b4c3439bba310bded9bed81ec2a59dc8c9c
Nikita Maslov <maslov.n.e@gmail.com>
2023-07-29 22:36:24 +0300

    init get_product method

--- 2_idioms/src/main.rs
+++ 2_idioms/src/main.rs
@@ -1,24 +1,42 @@
+use derive_more::Sub;
+use std::cmp::Reverse;
 use std::collections::HashMap;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 use thiserror::Error;
 
 type AmountOfProducts = u8;
 const MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND: AmountOfProducts = 10;
 
-const INITIAL_AMOUNT_OF_MONEY: AmountOfMoney = 100;
+const INITIAL_AMOUNT_OF_MONEY: [Coin; 15] = [
+    Coin::Fifty,
+    Coin::Twenty,
+    Coin::Twenty,
+    Coin::Ten,
+    Coin::Ten,
+    Coin::Five,
+    Coin::Five,
+    Coin::Two,
+    Coin::Two,
+    Coin::Two,
+    Coin::Two,
+    Coin::One,
+    Coin::One,
+    Coin::One,
+    Coin::One,
+];
 
-pub type AmountOfMoney = u64;
+type AmountOfMoneyPrimitive = u64;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Sub)]
+pub struct AmountOfMoney(AmountOfMoneyPrimitive);
+
 pub type ProductName = &'static str;
-pub type ProductPrice = AmountOfMoney;
 
 #[derive(Debug)]
 pub struct ProductInfo {
     name: ProductName,
-    price: ProductPrice,
-}
-
-#[derive(Debug)]
-struct InnerFullProduct {
-    info: ProductInfo,
+    price: AmountOfMoney,
     amount: AmountOfProducts,
 }
 
@@ -46,7 +64,7 @@ impl Product {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, EnumIter)]
 pub enum Coin {
     One = 1,
     Two = 2,
@@ -58,13 +76,13 @@ pub enum Coin {
 
 pub struct SuccessfulPurchase {
     pub product: Product,
-    pub rest: Vec<Coin>,
+    pub change: Vec<Coin>,
     pub vending_machine: VendingMachine<Idle>,
 }
 
 #[derive(Error, Debug)]
 pub enum PurchaseError {
-    #[error("not enough money: expected {expected}, got {got}")]
+    #[error("not enough money: expected {expected.0}, got {got.0}")]
     NotEnoughMoney {
         expected: AmountOfMoney,
         got: AmountOfMoney,
@@ -75,18 +93,33 @@ pub enum PurchaseError {
         product: ProductName,
         vending_machine: VendingMachine<InProcess>,
     },
+    #[error("cannot give rest")]
+    CannotGiveRest {
+        vending_machine: VendingMachine<InProcess>,
+    },
+}
+
+pub struct ResetResult {
+    pub vending_machine: VendingMachine<Idle>,
+    pub refund: Vec<Coin>,
+}
+
+impl From<&[Coin]> for AmountOfMoney {
+    fn from(value: &[Coin]) -> Self {
+        AmountOfMoney(value.iter().map(|c| *c as AmountOfMoneyPrimitive).sum())
+    }
 }
 
 #[derive(Debug)]
-struct Idle;
+pub struct Idle;
 
 #[derive(Debug)]
-struct InProcess(Vec<Coin>);
+pub struct InProcess(Vec<Coin>);
 
 #[derive(Debug)]
 pub struct VendingMachine<S> {
-    products: HashMap<ProductName, InnerFullProduct>,
-    earned_money: AmountOfMoney,
+    products: HashMap<ProductName, ProductInfo>,
+    earned_money: Vec<Coin>,
     state: S,
 }
 
@@ -94,80 +127,89 @@ impl Default for VendingMachine<Idle> {
     fn default() -> Self {
         Self {
             state: Idle,
-            earned_money: INITIAL_AMOUNT_OF_MONEY,
+            earned_money: Vec::from(INITIAL_AMOUNT_OF_MONEY),
             products: HashMap::from([
                 (
                     Product::KitKat.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::KitKat.as_str(),
-                            price: 35,
-                        },
+                    ProductInfo {
+                        name: Product::KitKat.as_str(),
+                        price: AmountOfMoney(35),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
                 (
                     Product::Oreo.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::Oreo.as_str(),
-                            price: 45,
-                        },
+                    ProductInfo {
+                        name: Product::Oreo.as_str(),
+                        price: AmountOfMoney(45),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
                 (
                     Product::Lays.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::Lays.as_str(),
-                            price: 50,
-                        },
+                    ProductInfo {
+                        name: Product::Lays.as_str(),
+                        price: AmountOfMoney(50),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
                 (
                     Product::Doritos.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::Doritos.as_str(),
-                            price: 50,
-                        },
+                    ProductInfo {
+                        name: Product::Doritos.as_str(),
+                        price: AmountOfMoney(50),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
                 (
                     Product::CocaCola.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::CocaCola.as_str(),
-                            price: 60,
-                        },
+                    ProductInfo {
+                        name: Product::CocaCola.as_str(),
+                        price: AmountOfMoney(60),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
                 (
                     Product::Pepsi.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::Pepsi.as_str(),
-                            price: 60,
-                        },
+                    ProductInfo {
+                        name: Product::Pepsi.as_str(),
+                        price: AmountOfMoney(60),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
                 (
                     Product::Water.as_str(),
-                    InnerFullProduct {
-                        info: ProductInfo {
-                            name: Product::Water.as_str(),
-                            price: 20,
-                        },
+                    ProductInfo {
+                        name: Product::Water.as_str(),
+                        price: AmountOfMoney(20),
                         amount: MAX_AMOUNT_OF_PRODUCTS_OF_ONE_KIND,
                     },
                 ),
             ]),
         }
+    }
+}
+
+impl<T> VendingMachine<T> {
+    /// 93
+    /// amount_of_50s = get_50s
+    ///
+    /// if amount_of_50s > 0:
+    ///    n = int(93 / 50)
+    ///    if n > amount_of_50s:
+    ///
+    ///
+    fn take_money_and_give_change(
+        &mut self,
+        inserted_coins: Vec<Coin>,
+        expected_rest: AmountOfMoney,
+    ) {
+        let mut available_coins = Coin::iter()
+            .map(|c| c as AmountOfMoneyPrimitive)
+            .collect::<Vec<_>>();
+        available_coins.sort_by_key(|c| Reverse(*c));
+
+        for coin in available_coins {}
     }
 }
 
@@ -189,11 +231,42 @@ impl VendingMachine<InProcess> {
     pub fn get_product(mut self, product: Product) -> Result<SuccessfulPurchase, PurchaseError> {
         let full_product = self.products.get_mut(product.as_str()).unwrap();
 
-        // if full_product.amount == 0 {
-        //     return Err(PurchaseError::ProductNotAvailable(product.as_str()));
-        // }
+        if full_product.amount == 0 {
+            return Err(PurchaseError::ProductNotAvailable {
+                product: product.as_str(),
+                vending_machine: self,
+            });
+        }
+
+        let product_price = full_product.price;
+        let inserted_money = AmountOfMoney::from(&*self.state.0);
+
+        if inserted_money < product_price {
+            return Err(PurchaseError::NotEnoughMoney {
+                expected: product_price,
+                got: inserted_money,
+                vending_machine: self,
+            });
+        }
+
+        let expected_rest = inserted_money - product_price;
 
         todo!()
+    }
+
+    pub fn insert_coins(&mut self, coins: Vec<Coin>) {
+        self.state.0.extend(coins);
+    }
+
+    pub fn reset(self) -> ResetResult {
+        ResetResult {
+            vending_machine: VendingMachine {
+                state: Idle,
+                products: self.products,
+                earned_money: self.earned_money,
+            },
+            refund: self.state.0,
+        }
     }
 }
 
