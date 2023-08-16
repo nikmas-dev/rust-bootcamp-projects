a39db1e1f20a4189bfedc81d34706d1ec117341e
Nikita Maslov <maslov.n.e@gmail.com>
2023-08-16 19:03:25 +0300

    finilize task

--- 3_ecosystem/3_1_testing/src/main.rs
+++ 3_ecosystem/3_1_testing/src/main.rs
@@ -1,4 +1,15 @@
+use rand::Rng;
+use std::fmt::{write, Display, Formatter};
 use std::{cmp::Ordering, env, io};
+
+type Number = u32;
+
+#[derive(Debug, PartialEq)]
+enum GameResult {
+    TooSmall,
+    TooBig,
+    Win,
+}
 
 fn main() {
     println!("Guess the number!");
@@ -15,28 +26,24 @@ fn main() {
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+        match play(secret_number, guess) {
+            GameResult::TooSmall => println!("Too small!"),
+            GameResult::TooBig => println!("Too big!"),
+            GameResult::Win => println!("You won!"),
         }
     }
 }
 
+fn play(secret_number: Number, guess: Number) -> GameResult {
+    match guess.cmp(&secret_number) {
+        Ordering::Less => GameResult::TooSmall,
+        Ordering::Greater => GameResult::TooBig,
+        Ordering::Equal => GameResult::Win,
+    }
+}
+
 fn get_secret_number() -> u32 {
-    let secret_number = env::args()
-        .skip(1)
-        .take(1)
-        .last()
-        .expect("No secret number is specified");
-    secret_number
-        .trim()
-        .parse()
-        .ok()
-        .expect("Secret number is not a number")
+    rand::thread_rng().gen_range(1..=100)
 }
 
 fn get_guess_number() -> Option<u32> {
@@ -45,4 +52,35 @@ fn get_guess_number() -> Option<u32> {
         .read_line(&mut guess)
         .expect("Failed to read line");
     guess.trim().parse().ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check(secret_number: Number, guess: Number, expected_result: GameResult) {
+        let game_result = play(secret_number, guess);
+        assert_eq!(game_result, expected_result);
+    }
+
+    #[test]
+    fn guessed_number_is_too_small() {
+        let secret_number = 10;
+        let guess = 5;
+        check(secret_number, guess, GameResult::TooSmall);
+    }
+
+    #[test]
+    fn guessed_number_is_too_big() {
+        let secret_number = 10;
+        let guess = 15;
+        check(secret_number, guess, GameResult::TooBig);
+    }
+
+    #[test]
+    fn guessed_number_is_correct() {
+        let secret_number = 10;
+        let guess = 10;
+        check(secret_number, guess, GameResult::Win);
+    }
 }
