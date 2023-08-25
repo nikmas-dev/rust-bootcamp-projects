084827ffcb869f40628b0bd480fed0e2fb6a3778
Nikita Maslov <maslov.n.e@gmail.com>
2023-08-25 13:34:32 +0300

    finilize task

--- 3_ecosystem/3_10_threads/src/main.rs
+++ 3_ecosystem/3_10_threads/src/main.rs
@@ -1,3 +1,40 @@
+use rayon::prelude::*;
+use std::thread;
+
+const CHANNEL_CAPACITY: usize = 10;
+
+const MATRIX_SIZE: usize = 4096;
+type Element = u8;
+type Matrix = Vec<Vec<Element>>;
+
+fn generate_matrix() -> Matrix {
+    (0..MATRIX_SIZE)
+        .map(|_| (0..MATRIX_SIZE).map(|_| rand::random()).collect())
+        .collect()
+}
+
+type Sum = u64;
+fn calc_matrix_sum(matrix: &Matrix) -> Sum {
+    matrix.par_iter().flatten().map(|elem| *elem as Sum).sum()
+}
+
 fn main() {
-    println!("Implement me!");
+    let (producer, consumer) = crossbeam_channel::bounded(CHANNEL_CAPACITY);
+
+    thread::scope(|scope| {
+        scope.spawn(|| loop {
+            let matrix = generate_matrix();
+            if producer.send(matrix).is_err() {
+                break;
+            }
+        });
+
+        for _ in 0..2 {
+            scope.spawn(|| loop {
+                let matrix = consumer.recv().unwrap();
+                let sum = calc_matrix_sum(&matrix);
+                println!("sum: {}", sum);
+            });
+        }
+    });
 }
