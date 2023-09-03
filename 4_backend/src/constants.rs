97155fce4606ed723b38239e1c46d5af967123e9
Nikita Maslov <maslov.n.e@gmail.com>
2023-09-03 17:20:08 +0300

    limits depth of queries

--- 4_backend/src/constants.rs
+++ 4_backend/src/constants.rs
@@ -1,1 +1,2 @@
 pub const JWT_EXPIRATION_HOURS: u64 = 1;
+pub const MAX_DEPTH: usize = 20;
--- 4_backend/src/graphql/schema.rs
+++ 4_backend/src/graphql/schema.rs
@@ -1,3 +1,4 @@
+use crate::constants::MAX_DEPTH;
 use crate::graphql::mutation::Mutation;
 use crate::graphql::query::Query;
 use crate::repositories::defs::user::UserRepository;
@@ -6,5 +7,7 @@ use async_graphql::{EmptySubscription, Schema};
 pub type AppSchema<U> = Schema<Query<U>, Mutation<U>, EmptySubscription>;
 
 pub fn build_schema<U: UserRepository>() -> AppSchema<U> {
-    Schema::build(Query::default(), Mutation::default(), EmptySubscription).finish()
+    Schema::build(Query::default(), Mutation::default(), EmptySubscription)
+        .limit_depth(MAX_DEPTH)
+        .finish()
 }
